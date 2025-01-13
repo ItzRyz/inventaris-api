fb1f9cbb11e0e1f7c8fbb2d5888a63b4d5789e21
Al Farizi <zetsura.official04@gmail.com>
2025-01-13 13:08:01 +0700

    13/01/2025 - Auth - Added signin, signout, session function Controller - Added routes for auth.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signin(Request $req)
+    {
+        if (!Auth::attempt($req->only('username', 'password'))) {
+            return response(['message' => __('auth.failed')], 422);
+        }
+
+        $token = $req->user()->createToken('client-app');
+        return ['token' => $token->plainTextToken];
+    }
+
+    public function signout(Request $req)
+    {
+        $req->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    }
+
+    public function session(Request $req)
+    {
+        return $req->user();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,100 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\InventoryDetailController;
+use App\Http\Controllers\PJController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StatusController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('/signin', [AuthController::class, 'signin']);
+    Route::group([
+        'middleware' => 'auth:sanctum'
+    ], function () {
+        Route::get('/session', [AuthController::class, 'session']);
+        Route::post('/signout', [AuthController::class, 'signout']);
+    });
+});
+
+Route::group([
+    'prefix' => 'user'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'pj'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [PJController::class, 'index']);
+    Route::get('/{id}', [PJController::class, 'show']);
+    Route::post('/', [PJController::class, 'store']);
+    Route::put('/{id}', [PJController::class, 'update']);
+    Route::delete('/{id}', [PJController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'category'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'status'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [StatusController::class, 'index']);
+    Route::get('/{id}', [StatusController::class, 'show']);
+    Route::post('/', [StatusController::class, 'store']);
+    Route::put('/{id}', [StatusController::class, 'update']);
+    Route::delete('/{id}', [StatusController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'product'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'inv'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/', [InventoryController::class, 'index']);
+    Route::get('/{id}', [InventoryController::class, 'show']);
+    Route::post('/', [InventoryController::class, 'store']);
+    Route::put('/{id}', [InventoryController::class, 'update']);
+    Route::delete('/{id}', [InventoryController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'invdt'
+    // 'middleware'=> 'auth:sanctum',
+], function () {
+    Route::get('/{hdid}', [InventoryDetailController::class, 'index']);
+    Route::get('/{id}', [InventoryDetailController::class, 'show']);
+    Route::post('/{hdid}', [InventoryDetailController::class, 'store']);
+    Route::put('/{id}', [InventoryDetailController::class, 'update']);
+    Route::delete('/{id}', [InventoryDetailController::class, 'destroy']);
+});
